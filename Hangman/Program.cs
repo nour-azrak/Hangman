198390726737e4768c7f3659be0edd00da92290b
z004ehhj <z004ehhj@DINA5CG3222QBP>
2026-09-20 10:27:56 +0200

    Add project files.

---
+++ Hangman/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Hangman
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine(" --");
+            Console.WriteLine("/  \\");
+            
+        }
+    }
+}
