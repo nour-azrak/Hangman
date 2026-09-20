3ff1a750e3807a8ab4701a6d75321a4292677c6f
Nour <nour.alazrak98@gmail.com>
2026-09-20 19:01:39 +0200

    compare user input with the randomly selected word

--- Hangman/Program.cs
+++ Hangman/Program.cs
@@ -7,6 +7,7 @@
             Console.WriteLine("Welcome to the Hangman game...\n A name of a random European city has been rendomly chosen for you to guess\n");
 
             const int MAXIMUM_NUMBER_OF_FAULTS = 5;
+            int maximumNumberOfFaults = MAXIMUM_NUMBER_OF_FAULTS;
 
             // Define the líst of all words
             List<string> listOfWordsToGuess = new List<string>();
@@ -20,18 +21,43 @@
             Random rnd = new Random();
             int randomWordIndexFromList = rnd.Next(0, listOfWordsToGuess.Count -1);
 
-            // Create a charachter list to contain the guess from the user
-            List<char> Guessedword = new List<char>();
-            
+            // Create a charachter list to store the right guesses from the user
+            List<char> Guessword = new List<char>();
+
+            List<char> randomlySelectedWord = listOfWordsToGuess[randomWordIndexFromList].ToList<char>();
+
             // Fill the charachter list with '_'
-            foreach (char Character in listOfWordsToGuess[randomWordIndexFromList])
+            foreach (char Character in randomlySelectedWord)
             {
-                Guessedword.Add('_');
+                Guessword.Add('_');
             }
 
-            while (MAXIMUM_NUMBER_OF_FAULTS != 0)
+            char guessedChar;
+
+            while (maximumNumberOfFaults != 0)
             {
-                Console.Write("Please input the charachter to guess");
+                Console.WriteLine($"\nYou have {maximumNumberOfFaults} attempts left");
+                Console.WriteLine("\nYour current guess: " + string.Concat(Guessword));
+                Console.Write("\nPlease input the charachter to guess. Make sure the first Guess is a capital letter: ");
+                guessedChar = Console.ReadKey().KeyChar;
+
+                if (!char.IsAsciiLetter(guessedChar))
+                {
+                    Console.WriteLine("\nplease enter a valid charachter (only Ascii letters)\n");
+                    continue;
+                }
+                if (!randomlySelectedWord.Contains(guessedChar))
+                {
+                    maximumNumberOfFaults--;
+                    
+                }
+                for (int i = 0; i < randomlySelectedWord.Count; i++)
+                {
+                    if(guessedChar == randomlySelectedWord[i])
+                    {
+                        Guessword[i] = guessedChar;
+                    }
+                }
 
             }
 
