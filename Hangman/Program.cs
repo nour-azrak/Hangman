72de7696ad874da71e8c19dd8631d6bc7cb447fc
Nour <nour.alazrak98@gmail.com>
2026-09-20 17:53:47 +0200

    Created list of words and generated random index for the word to guess

--- Hangman/Program.cs
+++ Hangman/Program.cs
@@ -4,10 +4,40 @@
     {
         static void Main(string[] args)
         {
-           
-            Console.WriteLine(" --");
-            Console.WriteLine("/  \\");
+            Console.WriteLine("Welcome to the Hangman game...\n A name of a random European city has been rendomly chosen for you to guess\n");
+
+            const int MAXIMUM_NUMBER_OF_FAULTS = 5;
+
+            // Define the líst of all words
+            List<string> listOfWordsToGuess = new List<string>();
+            listOfWordsToGuess.Add("Copenhagen");
+            listOfWordsToGuess.Add("Salzburg");
+            listOfWordsToGuess.Add("Budapest");
+            listOfWordsToGuess.Add("Thessaloniki");
+            listOfWordsToGuess.Add("Barcelona");
+
+            // Create a random index for the word to be guessed from the list
+            Random rnd = new Random();
+            int randomWordIndexFromList = rnd.Next(0, listOfWordsToGuess.Count -1);
+
+            // Create a charachter list to contain the guess from the user
+            List<char> Guessedword = new List<char>();
             
+            // Fill the charachter list with '_'
+            foreach (char Character in listOfWordsToGuess[randomWordIndexFromList])
+            {
+                Guessedword.Add('_');
+            }
+
+            while (MAXIMUM_NUMBER_OF_FAULTS != 0)
+            {
+                Console.Write("Please input the charachter to guess");
+
+            }
+
+
+
+
         }
     }
 }
