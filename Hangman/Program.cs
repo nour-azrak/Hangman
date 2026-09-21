f7fbe775d2abbcdb50fa6c424b9752b3af8ecb60
Nour <nour.alazrak98@gmail.com>
2026-09-21 23:10:13 +0200

    if condition added to stop recurring faults from decreasing the attempts.

--- Hangman/Program.cs
+++ Hangman/Program.cs
@@ -24,6 +24,7 @@
             // Create a charachter list to store the right guesses from the user
             List<char> Guessword = new List<char>();
 
+            // Store the randomly selected word in a separate variable for readability
             List<char> randomlySelectedWord = listOfWordsToGuess[randomWordIndexFromList].ToList<char>();
 
             // Fill the charachter list with '_'
@@ -34,9 +35,12 @@
 
             char guessedChar;
 
+            List<char> storedUserInput = new List<char>();
+
             while (maximumNumberOfFaults != 0)
             {
                 Console.WriteLine($"\nYou have {maximumNumberOfFaults} attempts left");
+                Console.WriteLine("Your last guesses : " + string.Concat(storedUserInput));
                 Console.WriteLine("\nYour current guess: " + string.Concat(Guessword));
                 Console.Write("\nPlease input the charachter to guess. There is no capital letters. The guessing squence is not relevant: ");
                 guessedChar = Console.ReadKey().KeyChar;
@@ -46,16 +50,17 @@
                     Console.WriteLine("\nplease enter a valid charachter (only Ascii letters)\n");
                     continue;
                 }
+                if (Guessword.Contains(guessedChar) || storedUserInput.Contains(guessedChar))
+                {
+                    Console.WriteLine("\nYou already guessed this charachter please enter another one\n");
+                    continue;
+                }
+                storedUserInput.Add(guessedChar);
                 if (!randomlySelectedWord.Contains(guessedChar))
                 {
                     maximumNumberOfFaults--;
                     Thread.Sleep(500);
                     Console.Clear();
-                    continue;
-                }
-                if (Guessword.Contains(guessedChar))
-                {
-                    Console.WriteLine("\nYou already guessed this charachter please enter another one\n");
                     continue;
                 }
                 for (int i = 0; i < randomlySelectedWord.Count; i++)
